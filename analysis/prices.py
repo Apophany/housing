0e6243e52aa338fbcec61e26557293b9cafcdda1
Apophany <bagnallcf1@hotmail.com>
2020-11-22 23:15:53 +0000

    Add ability to group by daily or monthly in the price graph

--- analysis/prices.py
+++ analysis/prices.py
@@ -1,6 +1,8 @@
 import os
+from datetime import datetime
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as mplt
+import matplotlib.dates as mdates
 import numpy as np
 import pandas as pd
 from matplotlib import ticker
@@ -14,44 +16,59 @@ __search_root_path = data_save_path_prefix + "/{region}"
 __search_path = data_save_path_prefix + "/{region}/{date}/{num_bedrooms}" + __file_name
 
 
+class Frequency:
+    DAILY = 'Daily'
+    MONTHLY = 'Monthly'
+
+
 def get_data(borough: Borough, num_bedrooms: int):
-    results = pd.DataFrame(columns=['date', 'avg_price', 'count'])
+    results = pd.DataFrame(columns=['date', 'price'])
     root = __search_root_path.format(region=borough.name)
     for date_entry in os.scandir(root):
         data_path = __search_path.format(region=borough.name, date=date_entry.name, num_bedrooms=num_bedrooms)
         if os.path.isfile(data_path):
             data = pd.read_csv(data_path)
-            results.loc[len(results)] = [date_entry.name, data['price'].mean(), len(data)]
+            data['date'] = datetime.strptime(date_entry.name, '%Y-%m-%d')
+            results = results.append(data[['date', 'price']], ignore_index=True)
 
-    return results
+    return results.set_index('date')
 
 
-def plot(loc: Borough, bedrooms: int):
+def plot(loc: Borough, bedrooms: int, groupby=Frequency.MONTHLY, start_date=datetime(2015, 1, 1)):
     results = get_data(loc, bedrooms)
-    results = results.tail(110)
+    results = results[results.index > start_date]
+    results.dropna(inplace=True)
 
-    x_axis_data = results.index
-    y_axis_data = results['avg_price']
+    freq = 'M' if groupby == Frequency.MONTHLY else 'D'
+    grouped_data = results.groupby(pd.Grouper(freq=freq), dropna=True)
+    grouped_prices = grouped_data.mean().dropna()
+    grouped_volume = grouped_data.count().loc[grouped_data.count()['price'] > 0]
 
-    poly_coefficients = np.polyfit(x_axis_data, y_axis_data, 3)
+    x_axis_data = grouped_prices.index
+    y_axis_data = grouped_prices['price']
+
+    poly_coefficients = np.polyfit(mdates.date2num(x_axis_data), y_axis_data, 3)
     trend_function = np.poly1d(poly_coefficients)
 
-    fig, ax = plt.subplots()
+    fig, ax = mplt.subplots()
     ax.set_title(loc.name + ' ' + str(bedrooms) + ' Bedrooms')
 
-    ax2 = ax.twinx()
-    ax2.set_ylabel('Daily Listed Volume', rotation=90, labelpad=15, fontdict={'fontsize': 16})
-    ax2.bar(x_axis_data, results['count'], width=0.3, color='gray', alpha=0.3)
+    title = 'Monthly' if groupby == Frequency.MONTHLY else 'Daily'
 
-    ax.set_ylabel('Average Daily Price', rotation=90, labelpad=15, fontdict={'fontsize': 16})
+    ax2 = ax.twinx()
+    ax2.set_ylabel(title + ' Listed Volume', rotation=90, labelpad=15, fontdict={'fontsize': 16})
+    ax2.bar(x_axis_data, grouped_volume['price'], width=0.3, color='gray', alpha=0.3)
+
+    ax.set_ylabel('Average ' + title + ' Price', rotation=90, labelpad=15, fontdict={'fontsize': 16})
     ax.set_xticks(x_axis_data)
-    ax.set_xticklabels(results['date'], rotation=90)
+    ax.set_xticklabels(grouped_prices.index, rotation=90)
     ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
+    ax.get_xaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: mdates.num2date(x).strftime('%d-%m-%Y')))
 
     ax.plot(x_axis_data, y_axis_data, color='blue')
-    ax.plot(x_axis_data, trend_function(x_axis_data), color='orange')
-    plt.show()
+    ax.plot(x_axis_data, trend_function(mdates.date2num(x_axis_data)), color='orange')
+    mplt.show()
 
 
 if __name__ == "__main__":
-    plot(loc=boroughs.HACKNEY, bedrooms=2)
+    plot(loc=boroughs.WALTHAM_FOREST, bedrooms=2, start_date=datetime(2020, 1, 1))
